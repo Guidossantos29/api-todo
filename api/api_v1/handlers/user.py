7ee78c9f76db85d3f0f52793a150ec633746b784
Luiz Guilherme <lguidossantos29@gmail.com>
2024-07-26 14:55:06 -0300

    feat: finish user authenticate

--- api/api_v1/handlers/user.py
+++ api/api_v1/handlers/user.py
@@ -1,17 +1,22 @@
-from fastapi import APIRouter,HTTPException,status
-import pymongo
+from fastapi import APIRouter, HTTPException, status, Depends
 from schemas.user_schema import UserAuth, UserDetail
 from services.user_service import UserService
-
+import pymongo
+from models.user_model import User
+from api.dependecies.user_deps import get_current_user
 
 user_router = APIRouter()
 
-@user_router.post('/adiciona',summary='adicionando usuario',response_model=UserDetail)
+@user_router.post('/adiciona', summary='Adiciona Usuário', response_model=UserDetail)
 async def adiciona_usuario(data:UserAuth):
     try:
         return await UserService.create_user(data)
-    except pymongo.errors.DuplicateKeyErrors:
+    except pymongo.errors.DuplicateKeyError:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail='Username ou email desse usuario ja existe'
+            detail='Username ou e-mail deste usuário já existe'
         )
+
+@user_router.get('/me', summary='Detalhes do Usuário Logado', response_model=UserDetail)
+async def get_me(user: User = Depends(get_current_user)):
+    return user
