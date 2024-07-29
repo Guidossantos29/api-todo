2cfef16cfa58f95a38406a6932a7a39535644a8b
Luiz Guilherme <lguidossantos29@gmail.com>
2024-07-29 07:55:19 -0300

    feat: created todo router

---
+++ api/api_v1/handlers/todo.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+
+
+todo_router =  APIRouter()
+
+@todo_router.get('test')
+async def teste():
+    return 'Notas'
--- api/api_v1/router.py
+++ api/api_v1/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from api.api_v1.handlers import user
 from api.auth.jwt import auth_router
+from api.api_v1.handlers import todo
 
 router = APIRouter()
 
@@ -15,3 +16,10 @@ router.include_router(
     prefix='/auth',
     tags=['auth']
 )
+
+router.include_router(
+    todo.todo_router,
+    prefix='/todo',
+    tags=['todo']
+)
+
