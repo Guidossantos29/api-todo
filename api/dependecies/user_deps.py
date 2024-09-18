b33d0dfb159a5d97f377b07105031ef032932a1c
Luiz Guilherme <lguidossantos29@gmail.com>
2024-09-18 03:07:48 -0300

    rafactor

--- api/dependecies/user_deps.py
+++ api/dependecies/user_deps.py
@@ -7,6 +7,7 @@ from schemas.auth_schema import TokenPayload
 from datetime import datetime
 from pydantic import ValidationError
 from services.user_service import UserService
+import logging
 
 oauth_reusavel = OAuth2PasswordBearer(
     tokenUrl=f"{settings.API_V1_STR}/auth/login",
@@ -20,14 +21,17 @@ async def get_current_user(token: str = Depends(oauth_reusavel)) -> User:
             settings.JWT_SECRET_KEY,
             settings.ALGORITHM
         )
+        logging.info(f"Token payload: {payload}")
         token_data = TokenPayload(**payload)
         if datetime.fromtimestamp(token_data.exp) < datetime.now():
+            logging.warning("Token expirado")
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail='Token expirado',
                 headers={'WWW-Authenticate': 'Bearer'}
             )
-    except (jwt.JWTError, ValidationError):
+    except (jwt.JWTError, ValidationError) as e:
+        logging.error(f"Erro na validação do token: {e}")
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail='Erro na validação do token',
@@ -35,8 +39,10 @@ async def get_current_user(token: str = Depends(oauth_reusavel)) -> User:
         )
 
     user = await UserService.get_user_by_id(token_data.sub)
+    logging.info(f"Usuário encontrado: {user}")
 
-    if not user:  # Certifique-se de verificar se o usuário existe
+    if not user:
+        logging.warning("Usuário não encontrado")
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail='Usuário não encontrado',
