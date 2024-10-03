3dfed7c53671273cde6d7f0fdc5912fb56999aaf
Luiz Guilherme <lguidossantos29@gmail.com>
2024-10-03 15:48:00 -0300

    refactor: todo update

--- services/todo_service.py
+++ services/todo_service.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-
 from fastapi import HTTPException
 from models.user_model import User
 from models.todo_model import Todo
@@ -23,21 +22,18 @@ class TodoService:
         todo = await Todo.find_one(Todo.todo_id == todo_id, Todo.owner.id == user.id)
         return todo
     
-    
-@staticmethod
-async def update_todo(user: User, todo_id: UUID, data: TodoUpdate):
-    todo = await TodoService.detail(user, todo_id)
-    if not todo:
-        raise HTTPException(status_code=404, detail="Todo not found")
-    
-    update_data = data.dict(exclude_unset=True)
-    update_data["updated_at"] = datetime.utcnow()  # Atualize o campo updated_at
-    
-    await todo.update({"$set": update_data})
-    await todo.save()
-    return todo
+    @staticmethod
+    async def update_todo(user: User, todo_id: UUID, data: TodoUpdate):
+        todo = await TodoService.detail(user, todo_id)
+        if not todo:
+            raise HTTPException(status_code=404, detail="Todo not found")
+        
+        update_data = data.dict(exclude_unset=True)
+        update_data["updated_at"] = datetime.utcnow()  # Atualize o campo updated_at
+        await todo.update({"$set": update_data})
+        await todo.save()
+        return todo    
 
-    
     @staticmethod
     async def delete_todo(user: User, todo_id: UUID) -> None:
         todo = await TodoService.detail(user, todo_id)
